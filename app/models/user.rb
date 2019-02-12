4cfcf74b52986feba106c4a6cb56d5579aec4204
Combos93 <combos93@mail.ru>
2019-02-12 17:35:56 +0600

    validations of user & questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ApplicationRecord
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
-
+  validates :username, length: { maximum: 40}
+  validates_format_of :username, :with => /\A[a-zA-Z0-9_]*\z/
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
 
   attr_accessor :password
 
