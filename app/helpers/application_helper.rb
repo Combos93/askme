1cfa9a4fd0a410eab2d886eab25fdb865957b307
Combos93 <combos93@mail.ru>
2019-02-15 16:25:20 +0600

    sklonenie to questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonenie(questions, vopros, voprosa, voprosov)
+    ostatok = questions % 10
+    if ostatok == 1
+      return vopros
+    end
+
+    if ostatok.between?(2, 4)
+      return voprosa
+    end
+
+    #if questions == 0
+    #  return "У вас нет вопросов."
+    #end
+
+    voprosov
+  end
 end
